a8c86ec1e70ad0fba10317aa09bbfc91fc76633b
SzipeRy <thannyapuesh@gmail.com>
2026-01-28 17:54:35 +0700

    feat: permu_before updated

--- 01-Brute-Force/a64_q1_permu_before/a64_q1_permu_before.cpp
+++ 01-Brute-Force/a64_q1_permu_before/a64_q1_permu_before.cpp
@@ -4,5 +4,16 @@
 
 int main(void)
 {
-    
+    std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+
+    int n, m;
+    int a, b;
+
+    std::cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> a >> b;
+        
+    }
 }
